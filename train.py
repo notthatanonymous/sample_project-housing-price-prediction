d87a27f4bf8197794714679674520520f5f25ef4
Musfiqur Rahman <musfiqurrahman.lab08@gmail.com>
2023-03-05 21:16:39 -0500

    adding random seed to rf

--- train.py
+++ train.py
@@ -76,7 +76,7 @@ def nn_model_training(X_train, y_train):
 
 
 def rf_model_training(X_train, y_train):
-    model = RandomForestRegressor()
+    model = RandomForestRegressor(random_state=42)
 
     model.fit(X_train, y_train)
 
